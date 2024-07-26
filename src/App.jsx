23366c0df4ea03ac4d752ed838d67b06658e22af
20FacuSam23 <facundo.samela@gmail.com>
2024-07-26 16:45:03 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import logo from '/logo.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="#" target="_blank">         
+          <img src={logo} classNemae="logo" alt="ava"/>
+        </a>       
+      </div>
+    </>
+  )
+}
+
+export default App
